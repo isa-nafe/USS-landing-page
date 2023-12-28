cb5429e0a6fd448a145004ce5382841451003b61
Isa <isa.nafieiev@gmail.com>
2023-12-28 14:58:56 -0500

    Initial commit

---
+++ src/test/java/uss/website/landingpage/LandingpageApplicationTests.java
@@ -0,0 +1,13 @@
+package uss.website.landingpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandingpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
